3d5186d140653fd9bc6e34e6bdc8de3f19211c7c
Christopher McIntosh <chrismac92@gmail.com>
2018-07-16 20:18:36 -0400

    firebase added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentDidMount() {
+    const config = {
+      apiKey: 'AIzaSyDjPe4gAnjc7Ee-4tBJbKsANC1yhiWQR4c',
+      authDomain: 'manager-be032.firebaseapp.com',
+      databaseURL: 'https://manager-be032.firebaseio.com',
+      projectId: 'manager-be032',
+      storageBucket: '',
+      messagingSenderId: '63601980787'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>Hello!</Text>
         </View>
